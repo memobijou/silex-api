53d0f05ee72eb43f09c4b1a7247c82442d097fe6
mbijou <mbijou@live.de>
2018-07-12 16:45:35 +0000

    slim

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 
-echo 'hello world 23';
-phpinfo();
+require 'vendor/autoload.php';
+
+$app = new Slim\App();
+
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    return $response->getBody()->write("Hello, " . $args['name']);
+});
+
+$app->run();
 
 ?>
